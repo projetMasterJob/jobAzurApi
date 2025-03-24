fc0ecdef9130369c4c76c6d55bad030459d9ef52
Mike <mickael.djegherif@outlook.fr>
2025-03-24 18:30:55 +0100

    with deploy

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+const indexRoutes = require('./src/routes/index');
+app.use('/api', indexRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
